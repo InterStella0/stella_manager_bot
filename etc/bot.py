bd962bce79279d73bb774d2e2d2320fbe4a63acf
Stella <sarahprog667@gmail.com>
2023-01-25 09:34:30 +0800

    use logging

--- etc/bot.py
+++ etc/bot.py
@@ -1,9 +1,10 @@
 import asyncio
 import glob
-import sys
+import logging
 import traceback
 from typing import Generator
 
+import asyncpg
 import discord
 import starlight
 from discord.ext import commands
@@ -16,7 +17,9 @@ class StellaBotHandler(commands.Bot):
     def __init__(self) -> None:
         intents = discord.Intents.all()
         super().__init__("uwu ", intents=intents, help_command=starlight.MenuHelpCommand(with_app_command=True))
-        self.config = Configuration
+        self.config: Configuration = Configuration
+        self.db: asyncpg.Pool | None = None
+        self.dpy_logger: logging.Logger = logging.getLogger('discord.client')
 
     @staticmethod
     def find_all_extensions(*, ignore=('_', '.')) -> Generator[str, None, None]:
@@ -31,19 +34,25 @@ class StellaBotHandler(commands.Bot):
             try:
                 await self.load_extension(utils.fp_extension(ext))
             except Exception:
-                print("Failure to load", ext, file=sys.stderr)
+                self.dpy_logger.error(f"Failure to load {ext}")
                 traceback.print_exc()
             else:
-                print("Loaded", ext)
+                self.dpy_logger.info(f"Loaded {ext}")
 
         exts = [asyncio.create_task(load(ext))
                 for ext in self.find_all_extensions()]
         await asyncio.gather(*exts)
 
     async def setup_hook(self) -> None:
+        self.db = await asyncpg.create_pool(
+            database=self.config.database,
+            user=self.config.database_username,
+            password=self.config.database_password
+        )
         await self.extension_loader()
 
     async def _establishing(self):
+        discord.utils.setup_logging()
         async with self:
             await self.start(self.config.token)
 
